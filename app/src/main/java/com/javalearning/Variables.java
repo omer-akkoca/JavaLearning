ce4c60fb629bacb1fa8f0cacf73fb4f1a61172ce
omer-akkoca <omerakkoca1042@gmail.com>
2026-02-04 10:28:03 +0300

    variables

---
+++ app/src/main/java/com/javalearning/Variables.java
@@ -0,0 +1,35 @@
+package com.javalearning;
+
+public class Variables {
+    public  static  void main(String[] args){
+        System.out.println("This is a test string");
+        int x = 2;
+        int y = 9;
+        System.out.println("x times y equals " + x * y);
+
+        int myInt = 20;
+        long myLong = 26;
+        double myDouble = 10.0;
+        float myFloat = 10.0f;
+
+        System.out.println("myInt: " + myInt);
+        System.out.println("myLong: " + myLong);
+        System.out.println("myDouble: " + myDouble);
+        System.out.println("myFloat: " + myFloat);
+
+        // String
+        String name = "Ömer";
+        String surname = "Akkoca";
+        String fullname = name + " " + surname;
+        System.out.println(fullname);
+
+        // Boolean
+        boolean isAlive = true;
+        isAlive = false;
+        System.out.println("isAlive = " + isAlive);
+
+        // Final
+        final int birthDate = 2000;
+        System.out.println("birthDate: " + birthDate);
+    }
+}
